7912cc2b6c8bb2d0d5629e7e6fe2e66b1626be35
Yudai Tanaka <ytnk531@gmail.com>
2021-02-14 06:16:29 +0900

    Add tweeting test

--- spec/jobs/twitter_post_job_spec.rb
+++ spec/jobs/twitter_post_job_spec.rb
@@ -1,9 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "TwitterPostJob" do
-  it 'post' do
-    user = User.new(twitter_token: 'twitter_token', twitter_secret: 'twitter_secret')
-    post = Post.new(content: 'ddfs', user: user)
+  let!(:stub) {
+    stub_request(:post, "https://api.twitter.com/1.1/statuses/update.json")
+      .to_return(
+        headers: {content_type: "application/json"},
+        body: <<~JSON
+          {
+            "id": 1050118621198921700,
+            "id_str": "1050118621198921728"
+          }
+        JSON
+      )
+  }
+
+  it "posts status with authentication information" do
+    user = User.new(twitter_token: "twitter_token", twitter_secret: "twitter_secret")
+    post = Post.new(content: "test message", user: user)
     TwitterPostJob.perform_now(post)
+
+    expect(WebMock).to have_requested(:post, "https://api.twitter.com/1.1/statuses/update.json")
+      .with(
+        body: {"status" => "test message"},
+        headers: {"Authorization" => /oauth_token="twitter_token"/}
+      )
   end
 end
