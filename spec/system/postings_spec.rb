60b0a0d15a81bb7e046fd30c028f0ec7f75d2337
Yudai Tanaka <ytnk531@gmail.com>
2021-03-20 15:30:01 +0900

    Fix test

--- spec/system/postings_spec.rb
+++ spec/system/postings_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Postings", type: :system do
     click_on "Login"
     visit new_post_path
     fill_in "post[content]", with: "Test posting."
-    fill_in "post[time]", with: "10/01/2021\t07:15"
+    fill_in "post[time]", with: "10/01/2021\t17:15"
 
     expect { click_on "Create Post" }.to(
       change { Post.count }.by(1).and(
@@ -16,11 +16,11 @@ RSpec.describe "Postings", type: :system do
 
     expect(Post.last).to have_attributes(
       content: "Test posting.",
-      time: "2021/10/1 7:15".in_time_zone
+      time: "2021/10/1 17:15".in_time_zone
     )
     expect(page).to have_content "Done"
     expect(page).to have_content "Test posting."
-    expect(page).to have_content "2021/10/1 7:15:00"
+    expect(page).to have_content "2021/10/1 17:15:00"
   end
 
   it "requests authentication to non-login user" do
@@ -36,10 +36,10 @@ RSpec.describe "Postings", type: :system do
     visit new_post_path
     click_on "Login"
     fill_in "post[content]", with: "Test posting."
-    fill_in "post[time]", with: "10/01/2021\t07:15"
+    fill_in "post[time]", with: "10/01/2021\t17:15"
     click_on "Create Post"
 
-    expect(page).to have_content "2021/10/1 7:15:00"
+    expect(page).to have_content "2021/10/1 17:15:00"
     expect(page).to have_content "Test posting."
   end
 end
